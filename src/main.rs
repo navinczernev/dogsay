c9963b2cbff6729637c90339c42dd39208caeae3
navinczernev <navinczernev@gmail.com>
2024-09-14 09:27:03 +0530

    Add clap to parse

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use clap::Parser;
+
+
+#[derive(Parser)]
+struct Options {
+    message: String,
+}
+
 fn main() {
-    let message = std::env::args().nth(1)
-                        .expect("Missing the message. Usage dogsay <message>");
+    let options = Options::parse();
+    let message = options.message;
     println!("{message}");
     println!("\\");
     println!(" \\");

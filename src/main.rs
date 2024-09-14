171ab783ca220d610753c6ff65da278330cab863
navinczernev <navinczernev@gmail.com>
2024-09-14 09:27:30 +0530

    Add dead option

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@ struct Options {
     #[clap(default_value="woof!")]
     /// What does the dog say?
     message: String,
+    #[clap(short='d', long="dead")]
+    /// Make the dog appear dead
+    dead: bool,
 }
 
 fn main() {
     let options = Options::parse();
     let message = options.message;
+    let eye = if options.dead { "x" } else { "0" };
     println!("{message}");
     println!("\\");
     println!(" \\");
     println!("  /^ ^\\");
-    println!(" / 0 0 \\");
+    println!(" / {eye} {eye} \\");
     println!(" V\\ Y /V");
     println!("  / - \\");
     println!("  |    \\");
